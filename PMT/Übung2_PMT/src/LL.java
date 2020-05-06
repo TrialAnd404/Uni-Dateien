6d8e14ac79b5ddb8b90bcb4ac142ae649fc1bc0d
TrialAnd404 <40753437+TrialAnd404@users.noreply.github.com>
2020-05-06 15:00:55 +0200

    Kek Commit

---
+++ PMT/Übung2_PMT/src/LL.java
@@ -0,0 +1,143 @@
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class LL<A>  {
+  private final A hd;
+  private final LL<A> tl;
+
+  public boolean isEmpty(){
+    return hd==null&&tl==null;
+  }
+
+  public LL(A hd, LL<A> tl) {
+    this.hd = hd;
+    this.tl = tl;
+  }
+  public LL() {
+    this(null,null);
+  }
+
+  public int size() {
+    if (isEmpty())
+      return 0;
+    return 1 + tl.size();
+  }
+
+  public A get(int i) {
+    return i==0?hd:tl.get(i-1);
+  }
+
+  boolean containsWith(Predicate<? super A> p) {
+    return false;
+  }
+
+  boolean contains(A el) {
+    return false;
+  }
+
+  boolean isPrefixOf(LL<A> that) {
+    return false;
+  }
+
+  LL<A> drop(int i){
+    return new LL<>();
+  }
+
+  LL<A> take(int i){
+    return new LL<>();
+  }
+
+
+  LL<A> sublist(int from, int length){
+    return new LL<>();
+  }
+
+  A last(){
+    return null;
+  }
+
+  LL<A> append(LL<A> that){
+    return new LL<>();
+  }
+
+  void forEach(Consumer<? super A> con){
+  }
+
+  LL<A> filter(Predicate<? super A> p){
+    return new LL<>();
+  }
+
+  <B> LL<B> map(Function<? super A, ? extends B> f){
+    return new LL<B>();
+  }
+
+  LL<A> reverse(){
+    return new LL<A>();
+  }
+
+  @SuppressWarnings("unchecked")
+  static <A> LL<A> create(A... es){
+    LL<A> result = new LL<A>();
+    for (int i=es.length-1;i>=0;i--){
+      result = new LL<A>(es[i],result);
+    }
+    return result;
+  }
+
+
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((hd == null) ? 0 : hd.hashCode());
+    result = prime * result + ((tl == null) ? 0 : tl.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj){
+      return true;
+    }
+    if (obj == null){
+      return false;
+    }
+    if (!(obj instanceof LL)){
+      return false;
+    }
+    LL other = (LL) obj;
+    if (hd == null) {
+      if (other.hd != null){
+        return false;
+      }
+    } else if (!hd.equals(other.hd)){
+      return false;
+    }
+    if (tl == null) {
+      if (other.tl != null){
+        return false;
+      }
+    } else if (!tl.equals(other.tl)){
+      return false;
+    }
+    return true;
+  }
+  @Override
+  public String toString(){
+    StringBuffer result = new StringBuffer("[");
+    boolean first = true;
+    for (LL<A> it = this;!it.isEmpty();it=it.tl){
+      if (first){
+        first = false;
+      } else{
+        result.append(", ");
+      }
+      result.append(it.hd);
+    }
+    result.append("]");
+    return result.toString();
+  }
+
+}
